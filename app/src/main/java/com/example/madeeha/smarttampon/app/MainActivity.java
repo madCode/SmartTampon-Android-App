3def5c47e1b697d1d10d6d6afc2f9166b8a463cc
madCode <madeehamfg@yahoo.com>
2015-05-05 12:39:24 -0700

    calendar v1
    adding a calendar library

--- app/src/main/java/com/example/madeeha/smarttampon/app/MainActivity.java
+++ app/src/main/java/com/example/madeeha/smarttampon/app/MainActivity.java
@@ -108,7 +108,7 @@ public class MainActivity extends Activity implements BluetoothAdapter.LeScanCal
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
